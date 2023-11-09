eae43e43a34b3ba8fd6eec1b7f36d50689356eb4
Fawas <fawaskp1010@gmail.com>
2023-11-09 11:36:23 +0530

    added some custom tailwind class, made registration page responsive, added min-width to the root div

--- src/pages/User/UserRegistration.jsx
+++ src/pages/User/UserRegistration.jsx
@@ -6,28 +6,20 @@ import {} from "postcss";
 function UserRegistration() {
   return (
     <div className="bg-black relative overflow-hidden opacity-90 flex flex-col justify-center align-middle items-center min-h-screen md:px-0 px-5">
-      <div className="mx-auto w-90pt md:w-65pt md:max-w-[120rem]">
+      <div className="mx-auto w-90pt md:w-65pt md:max-w-[70rem]">
         <BlurredCircle />
-        <h1 className="text-gray-200 text-4xl px-10 py-5 font-semibold">
-          Registrations
+        <h1 className="mx-auto sm:w-90pt text-gray-200 text-2xl text-center md:text-left md:text-3xl py-2  sm:py-5 font-semibold">
+          Registration
         </h1>
-        <Card
-          style={{ height: "10vh" }}
-          className="mx-auto  sm:w-90pt border border-gray-700 bg-black"
-        >
-          <CardBody className="flex gap-5 items-center">
-            <Input label="Email" size="lg"
-               containerProps={{ className: "w-40pt md:w-65pt" }}
-            />
-
-            <Input
-              label="Password"
-              type="password"
-              color="dark-gray"
-              size="lg"
-              containerProps={{ className: "w-40pt md:w-65pt" }}
-              />
-            <Typography className="mx-auto bg-yellow-700 px-4 py-1 rounded-md" variant="h6" color="blue-gray">
+        <Card className="mx-auto sm:w-90pt rounded-lg px-3 border border-gray-700 bg-gray-900 bg-opacity-40">
+          <CardBody className="flex flex-col  md-1:flex-row gap-5 overflow-x-clip items-center">
+            <Input label="Name" type="name" color="white" />
+            <Input label="Email" type="email" color="white" />
+            <Typography
+              className="mx-auto bg-yellow-700 px-4 py-1 rounded-md"
+              variant="h6"
+              color="black"
+            >
               Next
             </Typography>
           </CardBody>
